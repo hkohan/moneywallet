73e4ad0ddb5414c7cb751430d47858d1149475dc
hkohan <hk7597@gmail.com>
2019-09-19 22:18:17 +0430

    pager

--- app/src/main/java/com/oriondev/moneywallet/ui/adapter/pager/PieChartViewPagerAdapter.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/adapter/pager/PieChartViewPagerAdapter.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.adapter.pager;
 
 import android.support.annotation.NonNull;
@@ -29,9 +10,7 @@ import com.oriondev.moneywallet.R;
 import com.oriondev.moneywallet.model.PeriodDetailFlowData;
 import com.oriondev.moneywallet.ui.view.chart.PieChart;
 
-/**
- * Created by andrea on 14/08/18.
- */
+
 public class PieChartViewPagerAdapter extends PagerAdapter {
 
     private PeriodDetailFlowData mData;
