65dbd72a61d28a82814bfd10e179fbbf754ef0d0
hkohan <hk7597@gmail.com>
2019-09-19 22:57:21 +0430

    map

--- app/src/gmap/java/com/oriondev/moneywallet/view/MapViewWrapper.java
+++ app/src/gmap/java/com/oriondev/moneywallet/view/MapViewWrapper.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.view;
 
 import android.os.Bundle;
@@ -36,9 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by andrea on 16/10/18.
- */
+
 
 public class MapViewWrapper implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
