15293035a9a3dc9c781c847d0afd59bb8af79e3c
hkohan <hk7597@gmail.com>
2019-09-19 22:38:21 +0430

    fragment --> singlepanel

--- app/src/main/java/com/oriondev/moneywallet/ui/fragment/singlepanel/OverviewSinglePanelFragment.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/fragment/singlepanel/OverviewSinglePanelFragment.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.fragment.singlepanel;
 
 import android.content.BroadcastReceiver;
@@ -49,9 +30,7 @@ import com.oriondev.moneywallet.ui.adapter.pager.OverviewChartViewPagerAdapter;
 import com.oriondev.moneywallet.ui.adapter.recycler.OverviewItemAdapter;
 import com.oriondev.moneywallet.ui.fragment.base.SinglePanelFragment;
 
-/**
- * Created by andrea on 17/08/18.
- */
+
 public class OverviewSinglePanelFragment extends SinglePanelFragment implements OverviewSettingPicker.Controller, LoaderManager.LoaderCallbacks<OverviewData>, OverviewItemAdapter.Controller, CurrentWalletController {
 
     private static final int LOADER_OVERVIEW_DATA = 34848;
