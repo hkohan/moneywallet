281074516870b180771fd6c2de49a5a634f46c64
hkohan <hk7597@gmail.com>
2019-09-19 22:37:30 +0430

    fragment --> single

--- app/src/main/java/com/oriondev/moneywallet/ui/fragment/single/AboutFragment.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/fragment/single/AboutFragment.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.fragment.single;
 
 import android.content.ActivityNotFoundException;
@@ -53,9 +34,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-/**
- * Created by andrea on 31/03/18.
- */
+
 public class AboutFragment extends MaterialAboutFragment {
 
     private static final String TAG_CHANGE_LOG = "AboutFragment::Tag::ChangeLogDialog";
--- app/src/main/java/com/oriondev/moneywallet/ui/fragment/single/PeriodDetailFlowFragment.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/fragment/single/PeriodDetailFlowFragment.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.fragment.single;
 
 import android.content.BroadcastReceiver;
@@ -50,9 +31,7 @@ import com.oriondev.moneywallet.utils.MoneyFormatter;
 
 import java.util.Date;
 
-/**
- * Created by andrea on 01/05/18.
- */
+
 public class PeriodDetailFlowFragment extends Fragment implements PeriodDetailFlowAdapter.Controller, LoaderManager.LoaderCallbacks<PeriodDetailFlowData>,CurrentWalletController {
 
     private static final int LOADER_FRAGMENT_DATA = 3648;
