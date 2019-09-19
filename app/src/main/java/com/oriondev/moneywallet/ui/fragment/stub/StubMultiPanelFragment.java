30ec405488c40e3d0fd941bc14429147253f0f19
hkohan <hk7597@gmail.com>
2019-09-19 22:39:25 +0430

    fragment --> stub

--- app/src/main/java/com/oriondev/moneywallet/ui/fragment/stub/StubMultiPanelFragment.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/fragment/stub/StubMultiPanelFragment.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.fragment.stub;
 
 import android.graphics.Color;
@@ -34,10 +15,7 @@ import android.widget.TextView;
 import com.oriondev.moneywallet.R;
 import com.oriondev.moneywallet.ui.fragment.base.MultiPanelFragment;
 
-/**
- * This class is a stub implementation of a simple multi panel fragment and it is useful until
- * a fragment for each section is not ready
- */
+
 public class StubMultiPanelFragment extends MultiPanelFragment {
 
     @Override
