329e6054a0b0f15434cee4396bbe5ef362142f0e
hkohan <hk7597@gmail.com>
2019-09-19 22:44:59 +0430

    view --> text

--- app/src/main/java/com/oriondev/moneywallet/ui/view/text/MaterialEditText.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/view/text/MaterialEditText.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.view.text;
 
 import android.animation.ValueAnimator;
@@ -44,9 +25,7 @@ import com.oriondev.moneywallet.storage.cache.TypefaceCache;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by andrea on 27/01/18.
- */
+
 public class MaterialEditText extends android.support.v7.widget.AppCompatEditText implements ValueAnimator.AnimatorUpdateListener, View.OnTouchListener {
 
     private final static int ANIMATION_DURATION = 300;
--- app/src/main/java/com/oriondev/moneywallet/ui/view/text/Utils.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/view/text/Utils.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.view.text;
 
 import android.content.Context;
@@ -30,9 +11,6 @@ import android.support.v4.content.ContextCompat;
 import android.util.TypedValue;
 import android.view.View;
 
-/**
- * Created by andrea on 30/01/18.
- */
 /*package-local*/ class Utils {
 
     /*package-local*/ static float getPixels(int dp, Resources resources) {
