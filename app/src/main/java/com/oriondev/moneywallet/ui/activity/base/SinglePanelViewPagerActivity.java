37644db864afd61fda6adeab77a9ec5a02d8fd81
hkohan <hk7597@gmail.com>
2019-09-19 22:12:21 +0430

    base activity

--- app/src/main/java/com/oriondev/moneywallet/ui/activity/base/SinglePanelViewPagerActivity.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/activity/base/SinglePanelViewPagerActivity.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.activity.base;
 
 import android.os.Bundle;
@@ -31,9 +12,7 @@ import android.view.ViewGroup;
 
 import com.oriondev.moneywallet.R;
 
-/**
- * Created by andre on 17/03/2018.
- */
+
 public abstract class SinglePanelViewPagerActivity extends SinglePanelAppBarActivity implements ViewPager.OnPageChangeListener {
 
     private TabLayout mTabLayout;
