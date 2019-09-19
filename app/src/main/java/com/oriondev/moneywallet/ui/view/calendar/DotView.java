2cdff466fabcc7cf0b02e0dc0b3edc7793e0e23e
hkohan <hk7597@gmail.com>
2019-09-19 22:42:28 +0430

    view --> calendar

--- app/src/main/java/com/oriondev/moneywallet/ui/view/calendar/DotView.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/view/calendar/DotView.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.view.calendar;
 
 import android.annotation.TargetApi;
@@ -32,7 +13,6 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
 
-/** Simple view to draw a colored and sized dot */
 public class DotView extends View {
 
     private Paint paint;
