b979402f534df9991515ee51c0f0a63b9c912a62
hkohan <hk7597@gmail.com>
2019-09-19 22:24:24 +0430

    drawable

--- app/src/main/java/com/oriondev/moneywallet/ui/drawable/TextDrawable.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/drawable/TextDrawable.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.drawable;
 
 import android.graphics.Canvas;
@@ -32,10 +13,7 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.RectShape;
 import android.graphics.drawable.shapes.RoundRectShape;
 
-/**
- * This class has been imported from this repository: https://github.com/amulyakhare/TextDrawable
- * It has been modified to correctly recycle Typefaces and not waste memory.
- */
+
 public class TextDrawable extends ShapeDrawable {
 
     private final Paint textPaint;
