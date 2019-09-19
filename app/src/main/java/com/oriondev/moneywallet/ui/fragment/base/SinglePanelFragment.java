0dd021a79bd9a7f037e95fb44b24dd21ea5e25e5
hkohan <hk7597@gmail.com>
2019-09-19 22:26:14 +0430

    fragment --> base

--- app/src/main/java/com/oriondev/moneywallet/ui/fragment/base/SinglePanelFragment.java
+++ app/src/main/java/com/oriondev/moneywallet/ui/fragment/base/SinglePanelFragment.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2018.
- *
- * This file is part of MoneyWallet.
- *
- * MoneyWallet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MoneyWallet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MoneyWallet.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.oriondev.moneywallet.ui.fragment.base;
 
 import android.app.Activity;
@@ -38,9 +19,7 @@ import com.oriondev.moneywallet.R;
 import com.oriondev.moneywallet.ui.activity.ToolbarController;
 import com.oriondev.moneywallet.utils.Utils;
 
-/**
- * Created by andrea on 17/08/18.
- */
+
 public abstract class SinglePanelFragment extends Fragment implements Toolbar.OnMenuItemClickListener {
 
     private Toolbar mToolbar;
